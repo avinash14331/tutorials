20a25df603d34b89cee075c764285660fa6e29ef
Avinash <avinash14331@gmail.com>
2022-02-10 19:37:58 +0530

    Thread Creation Basic

--- src/com/learn/java/multithreading/basic/creation/CreateByRunnableInterface.java
+++ src/com/learn/java/multithreading/basic/creation/CreateByRunnableInterface.java
@@ -1,9 +1,14 @@
 package com.learn.java.multithreading.basic.creation;
 
 public class CreateByRunnableInterface {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(new RunnableThread());
+        System.out.println(thread.getState());
         thread.start();
+        System.out.println(thread.getState());
+        Thread.sleep(6000);
+        System.out.println(thread.getState());
+
 
     }
 }
@@ -15,6 +20,7 @@ class RunnableThread implements Runnable{
             System.out.println("In run method using Runnable Interface having i: "+i);
             try {
                 Thread.sleep(1000);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
